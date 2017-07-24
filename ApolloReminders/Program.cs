bd7ea052ddcf28dce8f15ac04045e4927404bc80
kmuralik <kmuralik@users.noreply.github.com>
2017-07-25 02:52:32 +0530

    Sample updated

--- ApolloReminders/Program.cs
+++ ApolloReminders/Program.cs
@@ -15,8 +15,34 @@ namespace ApolloReminders
             cc.DrawTopLine();
             cc.Write("Apollo Reminders", textPosition: ColoredConsole.TextPosition.Center, textStyle: ColoredConsole.TextStyle.SpacedCaps);
             cc.DrawSeparator(ColoredConsole.LineStyle.Double, ColoredConsole.LineStyle.Double);
-            cc.Write("Reminders to Run:",foreColor: ConsoleColor.Cyan);
-            cc.Write("4 reminders found", textPosition: ColoredConsole.TextPosition.Right, foreColor: ConsoleColor.Gray);
+            // get all reminders that need to run today            
+            cc.Write("[A] Get Today's Reminders",foreColor: ConsoleColor.Cyan);
+            // get the count and display
+            cc.Write("A total of 4 reminders found", textPosition: ColoredConsole.TextPosition.Left, tabStop: 1, foreColor: ConsoleColor.Gray);
+            cc.DrawSeparator(ColoredConsole.LineStyle.Double, ColoredConsole.LineStyle.Single);
+            // loop through each reminder
+            cc.Write("[B] Run each reminder", foreColor: ConsoleColor.Cyan);
+            // foreach
+            // display remindeer name and schedule time
+            var reminderName = "First Reminder"; var nextReminderDate = "2017-Jul-25 0800 hrs";
+            cc.Write($"01. {reminderName} : {nextReminderDate}", tabStop: 1, foreColor: ConsoleColor.Green);
+            // run the associated procedure to get instance details
+            cc.Write($"Found 2 instances", tabStop: 2, foreColor: ConsoleColor.Gray);
+            // foreach instance send reminder
+            var requestNo = "2017051400003";
+            cc.Write($"{requestNo} - Reminder Sent",tabStop: 3, foreColor: ConsoleColor.Red);
+            requestNo = "2017062300016";
+            cc.Write($"{requestNo} - Reminder Sent",tabStop: 3, foreColor: ConsoleColor.Red);
+            reminderName = "Second Reminder"; nextReminderDate = "2017-Jul-25 0800 hrs";
+            cc.Write($"01. {reminderName} : {nextReminderDate}", tabStop: 1, foreColor: ConsoleColor.Green);
+            // run the associated procedure to get instance details
+            cc.Write($"Found 2 instances", tabStop: 2, foreColor: ConsoleColor.Gray);
+            // foreach instance send reminder
+            requestNo = "2017051400003";
+            cc.Write($"{requestNo} - Reminder Sent", tabStop: 3, foreColor: ConsoleColor.Red);
+            requestNo = "2017062300016";
+            cc.Write($"{requestNo} - Reminder Sent", tabStop: 3, foreColor: ConsoleColor.Red);
+
             cc.DrawSeparator(ColoredConsole.LineStyle.Double, ColoredConsole.LineStyle.Single);
             cc.Write("End", textPosition: ColoredConsole.TextPosition.Center);
             cc.DrawBottomLine();
